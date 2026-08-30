c7c545116f9c80e8b96492f6919c451f3f7405c9
AbdelrahmanDeisty20 <abooda601@gmail.com>
2026-08-30 11:20:07 +0300

    feat: create CandidateProfileResource for structured API response serialization

--- app/Http/Resources/CandidateProfileResource.php
+++ app/Http/Resources/CandidateProfileResource.php
@@ -12,9 +12,9 @@ class CandidateProfileResource extends JsonResource
         return [
             'id'               => $this->id,
             'user'             => new UserResource($this->whenLoaded('user')),
-            'personal_info'    => new CandidatePersonalInfoResource($this),
-            'career_info'      => new CandidateCareerInfoResource($this),
-            'media'            => new CandidateMediaResource($this),
+            'personal_info'    => new CandidatePersonalInfoResource($this->whenLoaded('personalInfo')),
+            'career_info'      => new CandidateCareerInfoResource($this->whenLoaded('careerInfo')),
+            'media'            => new CandidateMediaResource($this->whenLoaded('media')),
             'profile_status'   => $this->status, // pending, approved, rejected
             'rejection_reason' => $this->rejection_reason,
             'created_at'       => $this->created_at?->toDateTimeString(),
